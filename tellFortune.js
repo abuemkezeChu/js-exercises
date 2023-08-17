0de1d874e2d6a4449cb1439141de483dc5a29474
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-17 15:48:52 +0100

    feat: function to tell a fortune

---
+++ tellFortune.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const tellFortune = (numberOfChildren, partnerName, location, job) => {
+  console.log(`You will be a ${job} in ${location}, and married to ${partnerName} with ${numberOfChildren} kids`);
+}
+
+tellFortune(4,'Berklin', 'United Kingdom', 'Sofwtware Engineer')
+tellFortune(6,'Dorine', 'Cameroon', 'Accountant')
+tellFortune(3,'Josepha', 'Rwanda', 'Product Designer')
